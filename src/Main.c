b04e8142ea8ca191bbddfa06bac8b8055d78f4f0
C-Reaper <gnu.c.reaper@gmail.com>
2026-01-08 19:19:29 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -2,15 +2,15 @@
 
 int main(int argc,char** argv){
     //if(argc < 2){
-    //    printf("[ASMRuntime]: Arguments -> Usage: irr <.iralx>\n");
+    //    printf("[ASMRuntime]: Arguments -> Usage: irr <.asmalx>\n");
     //    return 0;
     //}
 
-    ASMRuntime irr = ASMRuntime_New("./code/IntraASM.iralx","./bin");
+    ASMRuntime irr = ASMRuntime_New("./code/Main.asmalx","./bin");
     ASMRuntime_Print(&irr);
 
     Variable params[] = {
-        Variable_Make(".RETURN","int",(Number[]){ 0LL },sizeof(Number),-1,NULL,NULL),
+        Variable_Make("a","int",(Number[]){ 0LL },sizeof(Number),-1,NULL,NULL),
         VARIABLE_END
     };
     ASMRuntime_Function(&irr,"main",params);
